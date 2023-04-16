b1f575d8c6d289570d1161307acceab02164fa93
prynnekey <prynnekey@gmail.com>
2023-04-16 12:49:33 +0800

    中序遍历

---
+++ tree/order/in.go
@@ -0,0 +1,25 @@
+package order
+
+func inorderTraversal(root *TreeNode) (res []int) {
+	stack := make([]*TreeNode, 0)
+
+	for len(stack) > 0 || root != nil {
+		// 将左节点全部入栈
+		for root != nil {
+			stack = append(stack, root)
+
+			root = root.Left
+		}
+
+		// 出栈
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		// 加入结果集
+		res = append(res, root.Val)
+
+		root = root.Right
+	}
+
+	return res
+}
