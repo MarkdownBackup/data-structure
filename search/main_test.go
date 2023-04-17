e3bb3c993312c289c8d7c10d35c5add67b476565
prynnekey <prynnekey@gmail.com>
2023-04-17 13:52:03 +0800

    test case

--- search/main_test.go
+++ search/main_test.go
@@ -8,11 +8,13 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	nums := []int{-1, 0, 3, 5, 9, 12, 13, 15, 21}
+	a := []int{-1, 0, 3, 5, 9, 12, 13, 15, 21}
+	// a := []int{2}
 
-	// i := search.BinarySearch(nums, 9)
-	// i := search.InterpolationSearchRecursive(nums, 9)
-	i := search.InterpolationSearch(nums, 9)
+	// i := search.BinarySearch(a, 9)
+	// i := search.InterpolationSearchRecursive(a, 9)
+	// i := search.InterpolationSearch(a, 9)
+	i := search.FibonacciSearch(a, 12)
 
 	fmt.Printf("i: %v\n", i)
 
